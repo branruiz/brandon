c9cc48a055c9b889a881fbad3c760a8b219355f3
T-102 <T-102@PC255>
2017-11-13 19:41:14 -0600

    13/11/17

---
+++ lenguajes1/src/excepciones1/ExcepcionesTres.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones1;
+
+/**
+ *
+ * @author T-102
+ */
+public class ExcepcionesTres {
+    public static void main(String[] args) {
+        
+        String x[]={"hola", "mundo"};
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+    }
+    
+}
